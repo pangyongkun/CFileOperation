ef7198e983145d51d92557abb7551e06cf98bdc2
pangyongkun <1196189178@qq.com>
2017-07-16 00:12:15 +0800

    linux文件读写操作，open.c是具体操作，hello.c是用来测试的文件

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
-##	linux系统文件操作
+##	文件操作---系统调用
 
 ###	1.open
+
+	int open(const char *path, int oflags);
+
+	int open(const char *path, int oflags, mode_t mode );
+
+oflags常用：
+
+	O_RDONLY 以只读方式
+	O_WRONLY 以只写方式
+	O_RDWR   以读写方式打开
+	O_CREAT  不存在创建
+
+###	2.read
+
+	size_t read(int fildes, void *buf, size_t nbytes);
+
+	参数说明：
+	fildes：文件描述符，标识要读取的文件。如果为0，则从标准输入读数据。类似于scanf（）的功能。
+	*buf：缓冲区，用来存储读入的数据。
+	nbytes：要读取的字符数。
+	返回值：size_t返回成功读取的字符数，它可能会小于请求的字节数。
+
+
+###	3.write
+
+	size_t write(int fildes, const void *buf, size_t nbytes);
+
+	参数说明：
+	fildes：文件描述符，标识了要写入的目标文件。例如:fildes的值为1，就像标准输出写数据，也就是在显示屏上显示数据；如果为 2 ，则想标注错误写数据。
+	*buf：待写入的文件，是一个字符串指针。
+	nbytes：要写入的字符数。
+
+###	4.close
+
+	int close(int fildes);
+
 
 
--- open.c
+++ open.c
@@ -5,19 +5,48 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
 
 int main(void)
 {
 	int fd=-1;
+	int ret=-1;
+	char buf[32];
 
-	fd = open("hello.c",O_CREAT | O_TRUNC | O_WRONLY,00600);
+	//打开hello.c文件，如果不存在则创建
+	//fd = open("hello.c",O_CREAT | O_TRUNC | O_WRONLY,00600);
+
+	//这里 O_RDWR 是以读写方式打开
+	fd = open("hello.c",O_CREAT | O_RDWR,00600);
 	if(fd < 0){
 		perror("open:");
 		exit(1);
-	}
-	else
+	}else{
 		printf("open file:hello.c %d\n",fd);
+	}
 
+	//读取文件
+	ret = read(fd,buf,12);
+	if(ret<0)
+	{
+		printf("read err\n");
+	}else{
+		//为字符串添加结束标记
+		buf[ret]='\0';
+		//打印读取的字符串
+		printf("str=%s\n",buf);
+	}
+
+	//向文件中写入数据
+	ret = write(fd,"\ni can write!",strlen("\ni can write!"));
+	if(ret == strlen("\ni can write!")){
+		printf("successfully write\n");
+	}else{
+		printf("write erro...\n");
+	}
+
+	//关闭文件
 	if(close(fd) < 0)
 	{
 		perror("close:");
